88827b59c73f138d2dae5030b0bf1b6ef5fd7797
RiftomeR <endernetmc@gmail.com>
2018-06-17 20:12:25 +0300

    Made GUIs look a little bit better.

--- src/main/java/riftomer/bcex/gui/GuiPipeAdvIron.java
+++ src/main/java/riftomer/bcex/gui/GuiPipeAdvIron.java
@@ -2,6 +2,7 @@ package riftomer.bcex.gui;
 
 import buildcraft.lib.gui.*;
 import buildcraft.lib.gui.button.GuiButtonDrawable;
+import buildcraft.lib.gui.button.StandardSpriteButtons;
 import buildcraft.lib.gui.elem.GuiElementDrawable;
 import buildcraft.lib.gui.pos.*;
 import buildcraft.transport.BCTransportSprites;
@@ -9,33 +10,42 @@ import net.minecraft.util.ResourceLocation;
 import riftomer.bcex.gui.container.ContainerAdvIronPipe;
 
 public class GuiPipeAdvIron extends GuiBC8<ContainerAdvIronPipe> {
-    private static final GuiButtonDrawable.Builder WDGET_BUTTON_BUILDER = new GuiButtonDrawable.Builder(new GuiRectangle(20, 20), new GuiIcon(new ResourceLocation("buildcrafttransport:textures/gui/pipe_emzuli.png"), 176, 0, 20, 20, 256));
+    private static final ResourceLocation BG_TEX = new ResourceLocation("bcex:textures/gui/adviron.png");
+    private static final GuiIcon BG_ICON = new GuiIcon(BG_TEX, 0, 0, 60, 190);
 
     public GuiPipeAdvIron(ContainerAdvIronPipe container) {
         super(container);
+        xSize = 60;
+        ySize = 190;
     }
 
     @Override
     public void initGui() {
         super.initGui();
-        int yPos = 0;
+        int yPos = 5;
         for (ContainerAdvIronPipe.ColorWidget widget : container.widgets) {
-            addWidgetButton(widget, 0, yPos);
+            addWidgetButton(widget, 5, yPos);
             yPos += 30;
         }
     }
 
     private void addWidgetButton(ContainerAdvIronPipe.ColorWidget widget, int x, int y) {
         IGuiPosition pos = mainGui.rootElement.offset(x, y);
-        GuiButtonDrawable button = new GuiButtonDrawable(mainGui, widget.fromColor.name(), pos.offset(20,0), WDGET_BUTTON_BUILDER);
+        GuiButtonDrawable button = new GuiButtonDrawable(mainGui, widget.fromColor.name(), pos.offset(20, 0), StandardSpriteButtons.TINY_BUTTON_DRAWABLE);
         button.registerListener((b, u) -> widget.nextColor());
         mainGui.shownElements.add(button);
         mainGui.shownElements.add(button.createTextElement(">"));
         IGuiArea fromArea = new GuiRectangle(20, 20).offset(pos);
-        IGuiArea toArea = new GuiRectangle(20, 20).offset(pos).offset(40, 0);
+        IGuiArea toArea = new GuiRectangle(20, 20).offset(pos).offset(30, 0);
         ISimpleDrawable fromIcon = (_x, _y) -> GuiIcon.drawAt(BCTransportSprites.ACTION_PIPE_COLOUR[widget.fromColor.color.ordinal()], _x + 2, _y + 2, 16);
         ISimpleDrawable toIcon = (_x, _y) -> GuiIcon.drawAt(BCTransportSprites.ACTION_PIPE_COLOUR[widget.toColor.color.ordinal()], _x + 2, _y + 2, 16);
         mainGui.shownElements.add(new GuiElementDrawable(mainGui, fromArea, fromIcon, false));
         mainGui.shownElements.add(new GuiElementDrawable(mainGui, toArea, toIcon, false));
+    }
+
+    @Override
+    protected void drawBackgroundLayer(float partialTicks) {
+        BG_ICON.drawAt(mainGui.rootElement);
+        super.drawBackgroundLayer(partialTicks);
     }
 }
--- src/main/java/riftomer/bcex/gui/GuiPipeDistribution.java
+++ src/main/java/riftomer/bcex/gui/GuiPipeDistribution.java
@@ -2,6 +2,7 @@ package riftomer.bcex.gui;
 
 import buildcraft.lib.gui.*;
 import buildcraft.lib.gui.button.GuiButtonDrawable;
+import buildcraft.lib.gui.button.StandardSpriteButtons;
 import buildcraft.lib.gui.elem.GuiElementDrawable;
 import buildcraft.lib.gui.elem.GuiElementText;
 import buildcraft.lib.gui.pos.*;
@@ -10,14 +11,18 @@ import net.minecraft.util.ResourceLocation;
 import riftomer.bcex.gui.container.ContainerDistributionPipe;
 
 public class GuiPipeDistribution extends GuiBC8<ContainerDistributionPipe> {
-    private static final GuiButtonDrawable.Builder WIDGET_BUTTON_BUILDER = new GuiButtonDrawable.Builder(new GuiRectangle(20, 20), new GuiIcon(new ResourceLocation("buildcrafttransport:textures/gui/pipe_emzuli.png"), 176, 0, 20, 20, 256));
+    private static final ResourceLocation BG_TEX = new ResourceLocation("bcex:textures/gui/distrib.png");
+    private static final GuiIcon BG_ICON = new GuiIcon(BG_TEX, 0, 0, 100, 190);
+
     public GuiPipeDistribution(ContainerDistributionPipe container) {
         super(container);
-        int yPos = 0;
+        int yPos = 5;
         for (ContainerDistributionPipe.DistribWidget widget : container.widgets.values()) {
-            addWidget(widget, 0, yPos);
+            addWidget(widget, 5, yPos);
             yPos += 30;
         }
+        ySize = yPos + 5;
+        xSize = 100;
     }
 
     public void addWidget(ContainerDistributionPipe.DistribWidget widget, int x, int y){
@@ -26,17 +31,23 @@ public class GuiPipeDistribution extends GuiBC8<ContainerDistributionPipe> {
         IGuiArea colorArea = new GuiRectangle(20,20).offset(pos);
         mainGui.shownElements.add(new GuiElementDrawable(mainGui,colorArea,color,false));
         IGuiPosition posBD = pos.offset(20,0);
-        GuiButtonDrawable decrement = new GuiButtonDrawable(mainGui,"-",posBD,WIDGET_BUTTON_BUILDER);
+        GuiButtonDrawable decrement = new GuiButtonDrawable(mainGui, "-", posBD, StandardSpriteButtons.TINY_BUTTON_DRAWABLE);
         decrement.registerListener((b,u)-> widget.decrease());
         mainGui.shownElements.add(decrement);
         mainGui.shownElements.add(decrement.createTextElement("-"));
-        IGuiPosition posTXT = posBD.offset(25,0);
-        GuiElementText num = new GuiElementText(mainGui,posTXT.offset(0,6),()->widget.value + "", 0);
+        IGuiPosition posTXT = posBD.offset(15, 0);
+        GuiElementText num = new GuiElementText(mainGui, posTXT, () -> widget.value + "", 0);
         mainGui.shownElements.add(num);
         IGuiPosition posBI = posTXT.offset(()->num.getWidth() + 5,0);
-        GuiButtonDrawable increment = new GuiButtonDrawable(mainGui,"+",posBI,WIDGET_BUTTON_BUILDER);
+        GuiButtonDrawable increment = new GuiButtonDrawable(mainGui, "+", posBI, StandardSpriteButtons.TINY_BUTTON_DRAWABLE);
         increment.registerListener((b,u)->widget.increase());
         mainGui.shownElements.add(increment);
         mainGui.shownElements.add(increment.createTextElement("+"));
+    }
+
+    @Override
+    protected void drawBackgroundLayer(float partialTicks) {
+        BG_ICON.drawAt(mainGui.rootElement);
+        super.drawBackgroundLayer(partialTicks);
     }
 }
